4f9b6e6970385ca6834747fea37b577fb944d6f9
Ramesh <vashiramesh@gmail.com>
2020-04-18 20:10:44 +0530

    test

---
+++ ApiTestsSample/MGPUnitTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ApiTestsSample
+{
+
+    [TestClass]
+    public class MGPUnitTests
+    {
+
+        private static RestClient _client;
+
+        public MGPUnitTests()
+        {
+            _client = new RestClient();
+        }
+
+        private TestContext _testContext;
+
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set
+            {
+                _testContext = value;
+            }
+        }
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        private Uri GetBaseUrl(string method)
+        {
+            return new Uri($"{TestContext.Properties["ApiUrl"]}{method}");
+        }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            TestContext.Properties["ApiUrl"] = @"http://mgp-dev.intransgroup.com:88/";
+        }
+
+        [TestMethod]
+        public void GetVehicleClasses_ReturnsTrue()
+        {
+            _client.BaseUrl = GetBaseUrl("GetVehicleClasses");
+
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = _client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void GetAccountType_ReturnsTrue()
+        {
+            _client.BaseUrl = GetBaseUrl("GetAccountType");
+
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+
+            IRestResponse response = _client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void GetVehicleMakes_ReturnsTrue()
+        {
+            _client.BaseUrl = GetBaseUrl("GetVehicleMakes");
+
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("cache-control", "no-cache");
+
+            IRestResponse response = _client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void UserLogin_ReturnsTrue()
+        {
+            _client.BaseUrl = GetBaseUrl("UserLogin");
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("postman-token", "228c952c-d8d1-8062-39de-cdc2f4e72921");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\r\n  \"UserName\": \"ramesh.krishnan@emovis.us\",\r\n  \"Password\": \"Password123\"\r\n}", ParameterType.RequestBody);
+            IRestResponse response = _client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void MakePayment_ReturnsTrue()
+        {
+            _client.BaseUrl = GetBaseUrl("UserLogin");
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("postman-token", "228c952c-d8d1-8062-39de-cdc2f4e72921");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\r\n  \"UserName\": \"ramesh.krishnan@emovis.us\",\r\n  \"Password\": \"Password123\"\r\n}", ParameterType.RequestBody);
+            IRestResponse response = _client.Execute(request);
+
+
+            dynamic summary = JsonConvert.DeserializeObject(response.Content);
+
+            int accountId = summary.Account.AccountId;
+
+            _client.BaseUrl = GetBaseUrl("MakePayment");
+
+            request = new RestRequest(Method.POST);
+
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{\r\n  \"AccountId\": " + accountId + ",\r\n  \"CardId\": \"int\",\r\n  \"PaymentAmount\": 2.50,\r\n  \"Card\": {\r\n    \"Address\": {\r\n      \"AddressLine1\": \"11 Main St\",\r\n      \"City\": \"Westbury\",\r\n      \"PostalCode\": \"11590\",\r\n      \"CountryCode\": \"US\",\r\n    },\r\n    \"Name\": \"R K\",\r\n    \"CardNumber\": \"4012001037141112\",\r\n    \"CardType\": 1,\r\n    \"ExpirationMonth\": \"12\",\r\n    \"ExpirationYear\": \"2019\",\r\n    \"SecurityCode\": \"083\"\r\n  }\r\n}", ParameterType.RequestBody);
+            response = _client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+    }
+}
---
+++ ApiTestsSample/TLSForcedPlugin.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestsSample
+{
+    [Description("This plugin will force the underlying System.Net ServicePointManager to negotiate downlevel Tls12 instead of TLS. WARNING: The servers X509 Certificate will be ignored as part of this process, so verify that you are testing the correct system.")]
+    public class TLSForcedPlugin : WebTestPlugin
+    {
+        [Description("Enable or Disable the plugin functionality")]
+        [DefaultValue(true)]
+        public bool Enabled { get; set; }
+
+        public override void PreWebTest(object sender, PreWebTestEventArgs e)
+        {
+            base.PreWebTest(sender, e);
+
+            // We're using SSL3 here and not TLS. Without this line, nothing works.
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //we wire up the callback so we can override  behavior and force it to accept the cert
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCB;
+
+            //let them know we made changes to the service point manager
+            e.WebTest.AddCommentToResult(this.ToString() + " has made the following modification-> ServicePointManager.SecurityProtocol set to use Tls12 in WebTest Plugin.");
+        }
+        public static bool RemoteCertificateValidationCB(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            //If it is really important, validate the certificate issuer here.
+            //this will accept any certificate
+            return true;
+        }
+    }
+}
